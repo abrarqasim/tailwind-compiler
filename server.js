9b1a4bff3007d2ce4331577ecedbdf87bd8a7ec0
qasim <shannzyqasim@gmail.com>
2024-08-07 23:28:43 +0400

    added description and formatting

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/**
+ * @fileoverview Endpoint for compiling Tailwind CSS based on Gutenberg block classes.
+ *
+ * @description
+ * This file defines an Express.js endpoint that accepts a POST request with
+ * Gutenberg block classes, extracts the relevant Tailwind CSS classes,
+ * generates a temporary HTML file, runs Tailwind CSS compilation, and
+ * returns the compiled CSS with a developer comment prefixed.
+ *
+ * @version 1.0.0
+ * @date 2024-08-07
+ * @author Qasim
+ */
+
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const bodyParser = require("body-parser");
@@ -8,18 +22,23 @@ const filterGutenbergClasses = require("./gutenbergClassFilter");
 const app = express();
 
 const apiLimiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 1 minute
-	max: 10, // Limit each IP to 60 requests per windowMs
+	windowMs: 15 * 60 * 1000, // 15 minute
+	max: 10,
 	message: "Ay yo!😰 Too many requests from this IP, please try again after a minute 😐",
 	keyGenerator: (req, res) => {
-		return req.ip; // Use req.ip which will respect the trust proxy setting
+		return req.ip;
 	},
 });
 app.use(apiLimiter);
 app.use(bodyParser.json());
-const publicDir = path.join(__dirname, "public");
-if (!fs.existsSync(publicDir)) {
-	fs.mkdirSync(publicDir);
+
+const PORT = process.env.PORT || 3000;
+const PUBLIC_DIR = path.join(__dirname, "public");
+const TAILWIND_CONFIG = "./tailwind.config.js";
+const TEMP_FILE = path.join(PUBLIC_DIR, "temp.html");
+
+if (!fs.existsSync(PUBLIC_DIR)) {
+	fs.mkdirSync(PUBLIC_DIR);
 }
 
 app.post("/compile", (req, res) => {
@@ -32,12 +51,10 @@ app.post("/compile", (req, res) => {
 	// console.log("Received classes:", content);
 	// console.log("Extracted Tailwind classes:", tailwindClasses);
 	console.log("Adding classes in HTML markup...");
-	const tempFile = "./public/temp.html";
 	const htmlContent = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Temp</title></head><body><div class="${tailwindClasses}"></div></body></html>`;
-	const tailwindConfig = "./tailwind.config.js";
-	fs.writeFileSync(tempFile, htmlContent);
+	fs.writeFileSync(TEMP_FILE, htmlContent);
 	console.log("Compiling Tailwind CSS...🚀");
-	exec(`npx tailwindcss -c ${tailwindConfig} -o public/tailwind.css --content ${tempFile}`, (error, stdout, stderr) => {
+	exec(`npx tailwindcss -c ${TAILWIND_CONFIG} -o public/tailwind.css --content ${TEMP_FILE}`, (error, stdout, stderr) => {
 		if (error) {
 			console.error(`exec error: ${error}`);
 			return res.status(500).send("Internal Server Error");
@@ -53,7 +70,6 @@ app.post("/compile", (req, res) => {
 		});
 	});
 });
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.clear();
 	console.log(`🫶🏼 Server running on port ${PORT} 🫶🏼`);
